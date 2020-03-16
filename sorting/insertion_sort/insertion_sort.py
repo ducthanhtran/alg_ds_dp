b2634d92418da96af195777dac131dcf73c75eca
Duc Thanh Tran <duc.thanh.tran.dtt@gmail.com>
2020-03-16 16:39:18 +0100

    Added unittest for insertion sort (python)

--- sorting/insertion_sort/insertion_sort.py
+++ sorting/insertion_sort/insertion_sort.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 from typing import List
 import argparse
+import unittest
 
 
 def insertion_sort(integers: List[int]) -> List[int]:
@@ -15,10 +16,30 @@ def insertion_sort(integers: List[int]) -> List[int]:
     return integers
 
 
+class TestInsertionSort(unittest.TestCase):
+    def test_empty(self):
+        empty = []
+        self.assertEqual(insertion_sort(empty), [])
+    
+    def test_sorting(self):
+        a = [5]
+        self.assertEqual(insertion_sort(a), [5])
+
+        b = [1, 5, 2, -9]
+        self.assertEqual(insertion_sort(b), [-9, 1, 2, 5])
+        
+        c = [-1, -2, 0, -100]
+        self.assertEqual(insertion_sort(c), [-100, -2, -1, 0])
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('-i', type=int, nargs="+", help="Integer inputs")
+    parser.add_argument('--test', action="store_true", help="Run unittest only")
     args = parser.parse_args()
 
-    print("Input: {}".format(args.i))
-    print("Sorted output: {}".format(insertion_sort(args.i)))
+    if args.test:
+        unittest.main(argv=[__name__])
+    else:
+        print("Input: {}".format(args.i))
+        print("Sorted output: {}".format(insertion_sort(args.i)))
